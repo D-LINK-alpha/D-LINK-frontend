f1a1b65d147e1438677074402a852e7db3efe40e
daun-up <awhekdns@kookmin.ac.kr>
2024-04-04 01:12:16 +0900

    Test:환경 세팅 테스트

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import "./App.css"
-function App() {
-  return (
-      <div>
-          {/*tailwind 예시*/}
-        <h1 className="text-amber-900 text-5xl">hi</h1>
-          <p>브랜치 실험 중이에요</p>
-          <p>브랜치 살리기 살리기</p>
-      </div>
-  );
-}
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import OnboardingPage from './pages/OnboardingPage/onboardingPage';
 
+const App = () => {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<OnboardingPage />} />
+      </Routes>
+    </div>
+  );
+};
 export default App;
