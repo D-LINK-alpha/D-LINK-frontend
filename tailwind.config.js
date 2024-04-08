d5faaaf5b52de6ecec657718d9b58ed9d88e14d6
daun-up <awhekdns@kookmin.ac.kr>
2024-04-09 01:02:39 +0900

    Feat:onboardingPage 제작

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    // fontFamily: {
+    //   sans: ['Graphik', 'sans-serif'],
+    //   serif: ['Merriweather', 'serif'],
+    // },
+    extend: {
+      colors: {
+        'gray-color': '#868686' /*onboarding 작은 글씨 색상*/,
+      },
+    },
   },
   plugins: [],
-}
-
+};
