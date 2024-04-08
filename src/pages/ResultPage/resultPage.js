fbfb015e4063f96f63a2376c42305900716aa0d5
승준 <robin1415@kookmin.ac.kr>
2024-04-08 18:32:06 +0900

    Add:resultPage

---
+++ src/pages/ResultPage/resultPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+const ResultPage = () => {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+};
+
+export default ResultPage;
