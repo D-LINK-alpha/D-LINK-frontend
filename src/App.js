990e925eded3da9dd48eb113b3b339f34e98c842
daun-up <awhekdns@kookmin.ac.kr>
2024-04-09 01:17:41 +0900

    Feat:header,footer mui 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import OnboardingPage from './pages/OnboardingPage/onboardingPage';
+import OnboardingPage from './pages/OnboardingPage';
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<OnboardingPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
       </Routes>
     </div>
   );
