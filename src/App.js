b2e577c76adeb5784c5e7c36241ee4cc8426cd73
hyeonjin6530 <cola1876@naver.com>
2024-04-29 12:36:41 +0900

    Add: landing 페이지 제작

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import OnboardingPage from './pages/OnboardingPage/onboardingPage';
 import SignInPage from './pages/SignInPage/signInPage';
+import LandingPage from './pages/LandingPage/landingPage';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<OnboardingPage />} />
         <Route path="/signIn" element={<SignInPage />} />
+        <Route path="/landing" element={<LandingPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/LandingPage/landingPage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Item from '../../components/Item/item';
+
+const LandingPage = () => {
+  const dummyData = [
+    {drinkName: '그린티 라떼 더블샷', similarity:'98%', cafeName:'오설록', drinkType:'coffee'},
+    {drinkName: '쇼콜라 말차 모카', similarity:'80%', cafeName:'오설록', drinkType:'latte'},
+    {drinkName: '말차 초콜릿 라떼', similarity:'70%', cafeName:'오설록', drinkType:'latte'},
+    {drinkName: '밀크폼 그린티', similarity:'60%', cafeName:'오설록', drinkType:'tea'},
+  ];
+
+  return (
+    <div className="flex flex-col ">
+      <div className="flex items-end text-white text-[24px] pt-[18px] pl-[35px]">
+        Today&apos;s DLNK
+        <a href={'/'} className="text-[#DBDBDB] underline text-[12px] pb-[4px] pl-[100px]">더보기</a>
+      </div>
+      <div className="flex flex-col justify-center pt-[22px] pl-[23px] pr-[23px]">
+        {dummyData.map((item, index) => (
+          <Item
+            key={index}
+            drinkName={item.drinkName}
+            similarity={item.similarity}
+            cafeName={item.cafeName}
+            drinkType={item.drinkType}
+            size={index === 0 ? 'big' : 'small'}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default LandingPage;
