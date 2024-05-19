c4c7037345578e8a4077c4cc1a8000dcb5614e58
hyeonjin6530 <cola1876@naver.com>
2024-05-19 22:33:47 +0900

    Feat: 날짜 이동 기능 추가

--- src/pages/HistoryPage/historyPage.js
+++ src/pages/HistoryPage/historyPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../../components/Layout/Header/Header';
 import Footer from '../../components/Layout/Footer';
 import { ReactComponent as BackIcon } from '../../assets/back.svg';
@@ -9,26 +9,57 @@ import { ReactComponent as BackIcon } from '../../assets/back.svg';
 
 
 const HistoryPage = () => {
-  const today = new Date();
+  const [date, setDate] = useState(new Date());
+  const [formattedDate, setFormattedDate] = useState('');
   const week= ["일", "월", "화", "수", "목", "금", "토"];
-  const formattedDate = `${today.getFullYear()}. ${today.getMonth() + 1}. ${today.getDate()} ${week[today.getDay()]}요일 `;
+  const canGoBack = date > new Date(Date.now() - 7 * (24 * 60 * 60 * 1000));
+  const canGoForward = date < new Date().setHours(0, 0, 0, 0);
+
+  useEffect(() => {
+    const formatted = `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()} ${week[date.getDay()]}요일`;
+    setFormattedDate(formatted);
+  }, [date]);
+
+  const moveDate = (direction) => {
+    setDate((prevDate) => {
+      const newDate = new Date(prevDate);
+      newDate.setDate(prevDate.getDate() + direction);
+      return newDate;
+    });
+  };
+
 
   return (
     <div className="flex flex-col h-screen">
       <Header title="History" />
-      <div className="flex-1 mt-[64px]">
+      <div className="flex flex-col h-screen py-[64px]">
+      <div className="flex-1">
         <div className="flex flex-col w-[375px] h-[108px] bg-[#363636] pt-[24px]">
           <div className="flex justify-center items-center">
-            <BackIcon className="w-[8px] h-[11px] mr-[20px]"/>
-            <div className="flex flex-col justify-center w-[217px] h-[33px] rounded-[12px] bg-[#EDEDED] text-[15px] font-bold">
-              {formattedDate}
-            </div>
-            <BackIcon className="w-[8px] h-[11px] rotate-180 ml-[20px]"/>
+            <button
+              onClick={() => moveDate(-1)}
+              disabled={!canGoBack}
+              className="disabled:opacity-50"
+            >
+              <BackIcon className="w-[8px] h-[11px] mr-[20px]" />
+              </button>
+              <div
+                className="flex flex-col justify-center w-[217px] h-[33px] rounded-[12px] bg-[#EDEDED] text-[15px] font-bold">
+                {formattedDate}
+              </div>
+              <button
+                onClick={() => moveDate(1)}
+                disabled={!canGoForward}
+                className="disabled:opacity-50"
+              >
+                <BackIcon className="w-[8px] h-[11px] rotate-180 ml-[20px]" />
+                </button>
           </div>
           <div className="text-[12px] text-white font-normal pt-[15px]">
             추천 히스토리는 일주일 전 기록까지만 확인이 가능해요.
           </div>
         </div>
+      </div>
       </div>
       <Footer />
     </div>
