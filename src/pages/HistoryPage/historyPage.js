705a57ce6efe0cb978abb58ca7b23dac47801344
daun-up <awhekdns@kookmin.ac.kr>
2024-05-21 16:28:28 +0900

    feat: post 페이지

--- src/pages/HistoryPage/historyPage.js
+++ src/pages/HistoryPage/historyPage.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../components/Layout/Header/Header';
 import Footer from '../../components/Layout/Footer';
 import { ReactComponent as BackIcon } from '../../assets/back.svg';
@@ -8,16 +8,43 @@ const HistoryPage = () => {
   const [date, setDate] = useState(new Date());
   const [formattedDate, setFormattedDate] = useState('');
   const [filteredData, setFilteredData] = useState([]);
-  const week= ["일", "월", "화", "수", "목", "금", "토"];
+  const week = ['일', '월', '화', '수', '목', '금', '토'];
   const canGoBack = date > new Date(Date.now() - 7 * (24 * 60 * 60 * 1000));
   const canGoForward = date < new Date().setHours(0, 0, 0, 0);
 
   const dummyData = [
-    {drinkName: '그린티 라떼 더블샷', similarity:'98%', cafeName:'오설록', drinkType:'coffee', isLike:true, createdAt: "2024-05-20T06:34:15.666Z"},
-    {drinkName: '쇼콜라 말차 모카', similarity:'80%', cafeName:'오설록', drinkType:'latte', isLike:false, createdAt: "2024-05-20T06:34:15.666Z"},
-    {drinkName: '말차 초콜릿 라떼', similarity:'70%', cafeName:'오설록', drinkType:'latte', isLike:false, createdAt: "2024-05-20T06:34:15.666Z"},
-    {drinkName: '밀크폼 그린티', similarity:'60%', cafeName:'오설록', drinkType:'tea', isLike:true, createdAt: "2024-05-19T06:34:15.666Z"},
-
+    {
+      drinkName: '그린티 라떼 더블샷',
+      similarity: '98%',
+      cafeName: '오설록',
+      drinkType: 'coffee',
+      isLike: true,
+      createdAt: '2024-05-20T06:34:15.666Z',
+    },
+    {
+      drinkName: '쇼콜라 말차 모카',
+      similarity: '80%',
+      cafeName: '오설록',
+      drinkType: 'latte',
+      isLike: false,
+      createdAt: '2024-05-20T06:34:15.666Z',
+    },
+    {
+      drinkName: '말차 초콜릿 라떼',
+      similarity: '70%',
+      cafeName: '오설록',
+      drinkType: 'latte',
+      isLike: false,
+      createdAt: '2024-05-20T06:34:15.666Z',
+    },
+    {
+      drinkName: '밀크폼 그린티',
+      similarity: '60%',
+      cafeName: '오설록',
+      drinkType: 'tea',
+      isLike: true,
+      createdAt: '2024-05-19T06:34:15.666Z',
+    },
   ];
 
   useEffect(() => {
@@ -26,7 +53,7 @@ const HistoryPage = () => {
     setFormattedDate(formatted);
 
     // 날짜별로 데이터 분류
-    const filtered = dummyData.filter(item => {
+    const filtered = dummyData.filter((item) => {
       const itemDate = new Date(item.createdAt);
       return (
         itemDate.getFullYear() === date.getFullYear() &&
@@ -37,7 +64,6 @@ const HistoryPage = () => {
     setFilteredData(filtered);
   }, [date]);
 
-
   const moveDate = (direction) => {
     setDate((prevDate) => {
       const newDate = new Date(prevDate);
@@ -45,7 +71,6 @@ const HistoryPage = () => {
       return newDate;
     });
   };
-
 
   return (
     <div>
@@ -60,18 +85,17 @@ const HistoryPage = () => {
                 className="disabled:opacity-50"
               >
                 <BackIcon className="w-[8px] h-[11px] mr-[20px]" />
-                </button>
-                <div
-                  className="flex flex-col justify-center w-[217px] h-[33px] rounded-[12px] bg-[#EDEDED] text-[15px] font-bold">
-                  {formattedDate}
-                </div>
-                <button
-                  onClick={() => moveDate(1)}
-                  disabled={!canGoForward}
-                  className="disabled:opacity-50"
-                >
-                  <BackIcon className="w-[8px] h-[11px] rotate-180 ml-[20px]" />
-                  </button>
+              </button>
+              <div className="flex flex-col justify-center w-[217px] h-[33px] rounded-[12px] bg-[#EDEDED] text-[15px] font-bold">
+                {formattedDate}
+              </div>
+              <button
+                onClick={() => moveDate(1)}
+                disabled={!canGoForward}
+                className="disabled:opacity-50"
+              >
+                <BackIcon className="w-[8px] h-[11px] rotate-180 ml-[20px]" />
+              </button>
             </div>
             <div className="text-[12px] text-white font-normal pt-[15px]">
               추천 히스토리는 일주일 전 기록까지만 확인이 가능해요.
@@ -87,7 +111,9 @@ const HistoryPage = () => {
               />
             ))}
             {filteredData.length === 0 && (
-              <div className="text-white text-[16px] font-normal pt-[14px]">오늘 추천 받은 내역이 없어요.</div>
+              <div className="text-white text-[16px] font-normal pt-[14px]">
+                오늘 추천 받은 내역이 없어요.
+              </div>
             )}
           </div>
         </div>
