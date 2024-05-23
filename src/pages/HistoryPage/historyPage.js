69ec5b35769d56243d8283cd8eefd9f90eb5614c
hyeonjin6530 <cola1876@naver.com>
2024-05-23 18:16:48 +0900

    Design: 디자인 변경사항 반영 및 svg 컴포넌트 수정

--- src/pages/HistoryPage/historyPage.js
+++ src/pages/HistoryPage/historyPage.js
@@ -100,6 +100,7 @@ const HistoryPage = () => {
                         cafeName={item.cafeName}
                         drinkType={item.drinkType}
                         isLike={item.isLike}
+                        bookmark={item.bookmark}
                       />
                     ))}
                   </div>
@@ -111,6 +112,8 @@ const HistoryPage = () => {
                       <Item
                         key={index}
                         drinkName={item.drinkName}
+                        similarity={item.similarity}
+                        cafeName={item.cafeName}
                         drinkType={item.drinkType}
                         isLike={item.isLike}
                         bookmark={item.bookmark}
