794cc41b490034eccbb905d5cd1322ea74fccea2
hyeonjin6530 <cola1876@naver.com>
2024-05-27 03:34:05 +0900

    Feat: 메인페이지 api 연결

--- src/pages/LandingPage/landingPage.js
+++ src/pages/LandingPage/landingPage.js
@@ -3,20 +3,39 @@ import Item from '../../components/Item/item';
 import InfoBar from '../../components/Layout/Header/infoBar';
 import Footer from '../../components/Layout/Footer';
 import Modal from '../../components/Modal/modal';
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
 
 const LandingPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [mainData, setMainData] = useState([]);
+  const [nickname, setNickname] = useState('');
+  const [cookies] = useCookies(['token']);
 
-  const dummyData = [
-    {drinkName: '그린티 라떼 더블샷', similarity:'98%', cafeName:'오설록', drinkType:'coffee', isLike:true, bookmark:false, createdAt: "2024-05-27T06:34:15.666Z"},
-    {drinkName: '그린티 라떼 더블샷', similarity:'98%', cafeName:'오설록', drinkType:'latte', isLike:true, bookmark:false, createdAt: "2024-05-26T06:34:15.666Z"},
-    {drinkName: '그린티 라떼 더블샷', similarity:'98%', cafeName:'오설록', drinkType:'ade', isLike:true, bookmark:false, createdAt: "2024-05-23T06:34:15.666Z"},
-    {drinkName: '그린티 라떼 더블샷', similarity:'98%', cafeName:'오설록', drinkType:'tea', isLike:true, bookmark:false, createdAt: "2024-05-23T06:34:15.666Z"},
-    {drinkName: '그린티 라떼 더블샷', similarity:'98%', cafeName:'오설록', drinkType:'coffee', isLike:false, bookmark:true, createdAt: "2024-05-23T06:34:15.666Z"},
-    {drinkName: '쇼콜라 말차 모카', similarity:'80%', cafeName:'오설록', drinkType:'latte', isLike:false, bookmark:false, createdAt: "2024-05-23T06:34:15.666Z"},
-    {drinkName: '말차 초콜릿 라떼', similarity:'70%', cafeName:'오설록', drinkType:'latte', isLike:false, bookmark:true, createdAt: "2024-05-23T06:34:15.666Z"},
-    {drinkName: '밀크폼 그린티', similarity:'60%', cafeName:'오설록', drinkType:'tea', isLike:false, bookmark:true, createdAt: "2024-05-22T06:34:15.666Z"},
-  ];
+useEffect(() => {
+  const getMain = async () => {
+    const token = cookies.token;
+    console.log(token);
+    try{
+      const res = await axios.get(
+        `${process.env.REACT_APP_REST_API_URL}/api/main`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+      console.log('getMain res:', res);
+      setNickname(res.data.nickname);
+      setMainData(res.data.recommendHistory);
+    }catch (error){
+      console.error('error!!', error);
+      setMainData([]);
+    }
+  };
+
+  getMain();
+}, [cookies.token]);
+
 
   // 오늘 날짜의 데이터가 없으면 모달이 뜸
   useEffect(() => {
@@ -26,11 +45,11 @@ const LandingPage = () => {
     const day = String(today.getDate()).padStart(2, '0');
     const formattedToday = `${year}-${month}-${day}`;
 
-    const hasTodayData = dummyData.some(item => item.createdAt.split('T')[0] === formattedToday);
+    const hasTodayData = mainData.some(item => item.createdAt.split('T')[0] === formattedToday);
     if (!hasTodayData){
       setIsModalOpen(true);
     }
-  }, [dummyData]);
+  }, [mainData]);
 
   return (
     <>
@@ -47,19 +66,19 @@ const LandingPage = () => {
             </a>
           </div>
           <div className="flex flex-col justify-center pt-[22px] px-[23px]">
-            {dummyData.slice(0, 4).map((item, index) => (
+            {mainData.map((item, index) => (
               <Item
                 key={index}
-                drinkName={item.drinkName}
+                drinkName={item.beverage.name}
                 similarity={item.similarity}
-                cafeName={item.cafeName}
-                drinkType={item.drinkType}
+                cafeName={item.beverage.cafe}
+                drinkType={item.beverage.type}
                 size={index === 0 ? 'big' : 'small'}
                 createdAt={item.createdAt}
               />
             ))}
           </div>
-          <Modal isOpen={isModalOpen} name={'쿠민'} />
+          <Modal isOpen={isModalOpen} name={nickname} />
         </div>
       </div>
       <Footer />
